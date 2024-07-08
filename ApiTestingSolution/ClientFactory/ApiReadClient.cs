2ed05c39b1107070468d42813af758122bc13085
Anton_Mask <anton.maskalik@gmail.com>
2024-07-08 13:42:36 +0200

    Developed authenticators and implemented API clients

---
+++ ApiTestingSolution/ClientFactory/ApiReadClient.cs
@@ -0,0 +1,26 @@
+﻿using ApiTestingSolution.Authenticators;
+using ApiTestingSolution.Constants;
+using RestSharp;
+
+namespace ApiTestingSolution.ClientFactory
+{
+    public class ApiReadClient
+    {
+        private static RestClient? _writeClient;
+
+        public static RestClient GetRestClient()
+        {
+            if (_writeClient == null)
+            {
+                var options = new RestClientOptions(GlobalConstants.BaseUrl)
+                {
+                    Authenticator = new ReadScopeAuthenticator(GlobalConstants.UserName, 
+                    GlobalConstants.Password, GlobalConstants.BaseUrl),
+                };
+                _writeClient = new RestClient(options);
+            }
+
+            return _writeClient;
+        }
+    }
+}
---
+++ ApiTestingSolution/Constants/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiTestingSolution.Constants
+{
+    public static class GlobalConstants
+    {
+        public const string BaseUrl = "http://localhost:5040/";
+        public const string UserName = "0oa157tvtugfFXEhU4x7";
+        public const string Password = "X7eBCXqlFC7x-mjxG5H91IRv_Bqe1oq7ZwXNA8aq";
+    }
+}
